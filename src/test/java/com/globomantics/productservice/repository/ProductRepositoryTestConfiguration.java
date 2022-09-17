770d20238740e21bf2351f952f5ada65053a7261
kuanghc1 <kuanghc1@lenovo.com>
2022-09-17 20:20:04 +0800

    add repository test

--- src/test/java/com/globomantics/productservice/repository/ProductRepositoryTestConfiguration.java
+++ src/test/java/com/globomantics/productservice/repository/ProductRepositoryTestConfiguration.java
@@ -15,14 +15,11 @@ public class ProductRepositoryTestConfiguration {
     @Primary
     @Bean
     public DataSource dataSource() {
-
-        // Setup a data source for our tests
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
-
         return dataSource;
     }
 }
